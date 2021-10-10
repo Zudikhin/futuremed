89f9ae7d72208d937aa5394accccff0cd704d7f7
Zudikhin <Zudikhin>
2021-10-10 17:41:49 +0300

    final

--- js/main.js
+++ js/main.js
@@ -16,6 +16,104 @@ $(document).ready(function () {
         prevArrow: $('.conference_prev'),
         nextArrow: $('.conference_next')
     });
+
+    $(".header_content_right_registration").click(function() {
+        $(".modal_registration").addClass("active");
+        $(".back_modal").addClass("active");
+    });
+
+    $(".back_modal").click(function() {
+        $(this).removeClass("active");
+        $(".modal_registration").removeClass("active");
+    });
+
+    $(".modal_registration_close").click(function() {
+        $(".back_modal").removeClass("active");
+        $(".modal_registration").removeClass("active");
+    });
+
+    $(".program_reg").click(function() {
+        $(".modal_registration").addClass("active");
+        $(".back_modal").addClass("active");
+    });
+
+    $(".about_left_content_btns_registration").click(function() {
+        $(".modal_registration").addClass("active");
+        $(".back_modal").addClass("active");
+    });
+
+    $(".link_about").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(".about").offset().top
+        }, 2000);
+    });
+
+    $(".link_conference").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(".conference").offset().top
+        }, 2000);
+    });
+
+    $(".link_program").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(".program h2").offset().top
+        }, 2000);
+    });
+
+    $(".link_partners").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(".partners").offset().top
+        }, 2000);
+    });
     
+    $(".about_left_content_btns_program").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".program h2").offset().top
+        }, 2000);
+    });
+
+
+
+    $(".dropdown_link_about").click(function(e) {
+        e.preventDefault();
+        $(".header_content_right_btn").removeClass("active");
+        $(".header_dropdown").removeClass("active");
+        $('html, body').animate({
+            scrollTop: $(".about").offset().top
+        }, 2000);
+    });
+
+    $(".dropdown_link_conference").click(function(e) {
+        e.preventDefault();
+        $(".header_content_right_btn").removeClass("active");
+        $(".header_dropdown").removeClass("active");
+        $('html, body').animate({
+            scrollTop: $(".conference").offset().top
+        }, 2000);
+    });
+
+    $(".dropdown_link_program").click(function(e) {
+        e.preventDefault();
+        $(".header_content_right_btn").removeClass("active");
+        $(".header_dropdown").removeClass("active");
+        $('html, body').animate({
+            scrollTop: $(".program h2").offset().top
+        }, 2000);
+    });
+
+    $(".dropdown_link_partners").click(function(e) {
+        e.preventDefault();
+        $(".header_content_right_btn").removeClass("active");
+        $(".header_dropdown").removeClass("active");
+        $('html, body').animate({
+            scrollTop: $(".partners").offset().top
+        }, 2000);
+    });
+
+
 
 });
